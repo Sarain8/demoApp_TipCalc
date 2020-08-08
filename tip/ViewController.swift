64c798e12fe289e687494f6ee27c42dd0a3523fb
Sami Arain <sami.arain123@gmail.com>
2020-08-07 22:30:00 -0700

    Created storyboard Added bill, tip, and total features

--- tip/ViewController.swift
+++ tip/ViewController.swift
@@ -10,11 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func onTap(_ sender: Any) {
+        
+        view.endEditing(true  )
+    }
+    
+    @IBAction func calcTip(_ sender: Any) {
+        //get bill amount
+        let bill = Double(billField.text!) ?? 0
+        //calcualte tip and total
+        let tipPercentages = [0.15,0.2,0.25]
+        
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        //update tip and total labels
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        
+    }
 }
 
+ 
